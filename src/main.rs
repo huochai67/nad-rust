e849712097908dcfbd1af4c69d0600eb51487f62
Nicefish4520 <kcass774@gmail.com>
2024-02-29 23:22:06 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,38 @@
+use env_logger::{Env, Target};
 use nad_rust::run_loop;
+
+struct Mypipe{
+    pub data : String,
+}
+
+impl Mypipe {
+    fn new() -> Mypipe{
+        Mypipe{data : String::new()}
+    }
+}
+
+impl std::io::Write for Mypipe {
+    fn write(&mut self, buf: &[u8]) -> Result<usize, std::io::Error>{
+        let s = match std::str::from_utf8(buf) {
+            Ok(v) => v,
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+        self.data += s;
+        println!("{}", self.data);
+        Ok(buf.len())
+    }
+    fn flush(&mut self) -> Result<(), std::io::Error>{
+        self.data.clear();
+        Ok(())
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    let env = env_logger::Env::default()
-        .filter_or("RUST_LOG", "info")
-        .write_style_or("RUST_LOG_STYLE", "always");
-
-    env_logger::init_from_env(env);
+    //let pipe = Mypipe::new();
+    env_logger::Builder::from_env(Env::default().filter_or("RUST_LOG", "info")).target(Target::Stdout).init();
 
     log::info!("Program Start.");
+
     let _ = run_loop().await;
 }
