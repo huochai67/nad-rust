176747115494dbb99653fab0b19089ebeaa10cee
Nicefish4520 <kcass774@gmail.com>
2024-03-01 18:39:48 +0800

    more rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,53 @@
 use std::{process, time::Duration};
 
 use log::{debug, error, info, trace};
-use nad::nad::trial;
+use nad::nad::{trial, Config};
 
-pub async fn run_once(backdoor : & mut bool) {
+pub async fn run_once(backdoor: &mut bool) -> Result<(), Box<dyn std::error::Error>> {
     trace!("loop start");
-    let config = nad::nad::try_get_config(
+    trace!("try get config and test connection");
+    let config_opt: Option<nad::nad::Config>;
+    match nad::nad::try_get_config(
         "http://157.255.138.34/generate_204",
         "connectivitycheck.platform.hicloud.com",
     )
     .await
-    .unwrap_or_else(|err| {
-        error!("Problem testing connection: {}", err);
-        process::exit(1);
-    });
-
-    let nadauth = nad::nad::NadAuth::url("https://nad-worker.pinkfish.workers.dev".to_string());
-    if config.is_some() {
-        info!("Network Disconnected, trying trial now.");
-        trial(&config.unwrap()).await.unwrap_or_else(|err| {
-            error!("Problem sending trial: {}", err);
-            process::exit(1);
-        });
-        info!("Trail() return, Authed!");
-    } else {
-        if *backdoor {
-            trace!("Checking version");
-            match nadauth.check_version().await {
-                Ok(_) => {}
-                Err(err) => {
-                    error!("Problem checking version: {}", err);
-                    process::exit(1);
-                }
-            }
-    
-            trace!("verify device");
-            let machine_id = mid::get("mykey").unwrap();
-            debug!("get mid {}", machine_id);
-            match nadauth.verify_mid("mid").await {
-                Ok(_) => {}
-                Err(err) => {
-                    error!("Problem verify device: {}", err);
-                    process::exit(1);
-                }
-            }
-            *backdoor = false;
+    {
+        Ok(ret) => config_opt = ret,
+        Err(err) => {
+            return Err(err);
         }
-
-        trace!("loop end.");
     }
+
+    match config_opt {
+        Some(config) => {
+            info!("Network Disconnected, trying trial now.");
+            trial(&config).await?;
+            info!("Trail() return, Authed!");
+        }
+        None => {
+            if *backdoor {
+                trace!("Checking version");
+                let nadauth =
+                    nad::nad::NadAuth::url("https://nad-worker.pinkfish.workers.dev".to_string());
+                nadauth.check_version().await?;
+
+                trace!("verify device");
+                let machine_id = mid::get("mykey").unwrap();
+                debug!("get mid {}", machine_id);
+                nadauth.verify_mid(&machine_id).await?;
+                *backdoor = false;
+            }
+        }
+    }
+    trace!("loop end.");
+    Ok(())
 }
 
-pub async fn run_loop() {
+pub async fn run_loop()-> Result<(), Box<dyn std::error::Error>> {
     let mut backdoor = true;
     loop {
-        run_once(&mut backdoor).await;
+        run_once(&mut backdoor).await?;
         std::thread::sleep(Duration::from_secs(1));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 use env_logger::{Env, Target};
+use log::{error, info};
 use nad_rust::run_loop;
 
 #[tokio::main]
 async fn main() {
-    //let pipe = Mypipe::new();
     let mut default_log_level = "info";
     if cfg!(debug_assertions) {
         default_log_level = "trace";
     }
     env_logger::Builder::from_env(Env::default().filter_or("RUST_LOG", default_log_level)).target(Target::Stdout).init();
 
-    log::info!("Program Start.");
+    info!("Program Start.");
 
-    let _ = run_loop().await;
+    if let Err(err) = run_loop().await{
+        error!("error: {}", err);
+        std::process::exit(1);
+    }
 }
