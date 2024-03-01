c5acd834a928d4d2c35b6a61125b9f00b7c4cb4f
Nicefish4520 <kcass774@gmail.com>
2024-03-01 18:14:01 +0800

    set default log level to trace when using debug cfg

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,14 @@
 use env_logger::{Env, Target};
 use nad_rust::run_loop;
 
-struct Mypipe{
-    pub data : String,
-}
-
-impl Mypipe {
-    fn new() -> Mypipe{
-        Mypipe{data : String::new()}
-    }
-}
-
-impl std::io::Write for Mypipe {
-    fn write(&mut self, buf: &[u8]) -> Result<usize, std::io::Error>{
-        let s = match std::str::from_utf8(buf) {
-            Ok(v) => v,
-            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-        };
-        self.data += s;
-        println!("{}", self.data);
-        Ok(buf.len())
-    }
-    fn flush(&mut self) -> Result<(), std::io::Error>{
-        self.data.clear();
-        Ok(())
-    }
-}
-
 #[tokio::main]
 async fn main() {
     //let pipe = Mypipe::new();
-    env_logger::Builder::from_env(Env::default().filter_or("RUST_LOG", "info")).target(Target::Stdout).init();
+    let mut default_log_level = "info";
+    if cfg!(debug_assertions) {
+        default_log_level = "trace";
+    }
+    env_logger::Builder::from_env(Env::default().filter_or("RUST_LOG", default_log_level)).target(Target::Stdout).init();
 
     log::info!("Program Start.");
 
