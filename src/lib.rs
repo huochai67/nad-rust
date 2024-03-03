192ccf0a92ffa6a64a876d90b2a7761ca6bb9457
Nicefish4520 <kcass774@gmail.com>
2024-03-03 19:41:02 +0800

    remove lib mid

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,30 @@
-use std::time::Duration;
+use std::{
+    collections::hash_map::DefaultHasher,
+    hash::{Hash, Hasher},
+    time::Duration,
+};
 
 use log::{debug, info, trace};
+use mac_address2::get_mac_address;
 use nad::{
     nadauth::NadAuth,
     remoteconfig::{try_get_config, RemoteConfig},
     Client,
 };
+
+pub fn get_mid() -> Result<String, Box<dyn std::error::Error>> {
+    match get_mac_address() {
+        Ok(mac) => match mac {
+            Some(mac) => {
+                let mut s = DefaultHasher::new();
+                mac.to_string().hash(&mut s);
+                Ok(s.finish().to_string())
+            }
+            None => Err(Box::from("cant get mac address")),
+        },
+        Err(e) => Err(Box::from(e)),
+    }
+}
 
 pub async fn run_once(backdoor: &mut bool) -> Result<(), Box<dyn std::error::Error>> {
     trace!("try get config and test connection");
@@ -31,10 +50,17 @@ pub async fn run_once(backdoor: &mut bool) -> Result<(), Box<dyn std::error::Err
                 nadauth.check_version().await?;
 
                 trace!("verify device");
-                let machine_id = mid::get("NAD").unwrap();
-                debug!("get mid {}", machine_id);
-                nadauth.verify_mid(&machine_id).await?;
-                *backdoor = false;
+                match get_mid() {
+                    Ok(mid) => {
+
+                        debug!("get mid {}", mid);
+                        nadauth.verify_mid(&mid).await?;
+                        *backdoor = false;
+                    }
+                    Err(e) => {
+                        return Err(e);
+                    }
+                }
             }
         }
     }
